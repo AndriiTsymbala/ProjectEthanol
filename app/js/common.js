b971f7bf79a59bb36018b1cf0b83c1f889bd236a
AndriiTsymbala <andrii.tsymbala@gmail.com>
2019-04-03 23:12:28 +0300

    'fourth'

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,18 @@
 $(function() {
     //$('body').fadeOut();
 });
+
+$(document).ready(function(){
+
+	$(window).scroll(function() {
+		if($(this).scrollTop() != 0) {
+			$('.back-to-top').fadeIn();
+		} else {
+			$('.back-to-top').fadeOut();
+		}
+	});
+
+	$('.back-to-top').click(function() {
+		$('body, html').animate({scrollTop:0},800);
+    });
+});
